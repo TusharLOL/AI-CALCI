5298b015eca1d1cc4ff316bbe5b44a37deb8fb59
Tushar Dontulwar <tushdontulwar@gmail.com>
2024-11-29 21:44:48 +0530

    first commit

---
+++ constants.ts
@@ -0,0 +1,14 @@
+const SWATCHES = [
+    '#FF0000',  // red
+    '#FFA500',  // orange
+    '#FFFF00',  // yellow
+    '#008000',  // green
+    '#0000FF',  // blue
+    '#4B0082',  // indigo
+    '#EE82EE',  // violet
+
+    '#FFC0CB',  // pink
+    '#FFFFFF',  // white
+    ];
+
+export { SWATCHES };
